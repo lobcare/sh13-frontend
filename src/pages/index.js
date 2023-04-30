da20e751fb7badcf96abcb9f38c1fa2173d02da2
babyforever2022 <makpushkin1224@gmail.com>
2023-04-30 19:05:45 +0900

    pages work

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { Home } from "./Home";
+export { Login } from "./Login";
+export { Register } from "./Register";
+export { Profile } from "./Profile";
+export { Setting } from "./Setting";
+export { Usermanage } from "./Usermanage";
+export { Email } from "./Email";
